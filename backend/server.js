f75d347e7c4123fa146ce4a98d26aa6ff68dd6a7
root <badalpatidar210@gmail.com>
2020-09-06 21:35:27 +0530

    POrt binded

--- backend/server.js
+++ backend/server.js
@@ -25,21 +25,7 @@ app.use(cors());
 app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
-// app.get("/api/products", (req, res) => {
-//   res.send(data.products);
-// });
 
-// app.get("/api/product/:id", (req, res) => {
-//   const productId = req.params.id;
-//   const product = data.products.find((x) => x._id === +productId);
-
-//   if (product) {
-//     res.send(product);
-//   } else {
-//     res.status(404).send({ msg: "Product Not Found" });
-//   }
-// });
-
-app.listen(5000, () => {
+app.listen(process.env.PORT || 5000, () => {
   console.log("Server started at http://localhost:5000");
 });
