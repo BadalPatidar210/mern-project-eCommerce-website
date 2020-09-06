79fb95372ad718849779be9e877bef0834f276ed
root <badalpatidar210@gmail.com>
2020-09-06 21:40:26 +0530

    port updated

--- backend/server.js
+++ backend/server.js
@@ -8,6 +8,7 @@ import productRoute from "./routes/productRoute";
 import orderRoute from "./routes/orderRoute";
 // db connectiion
 const mongodbUrl = config.MONGODB_URL;
+const PORT = process.env.PORT || 5000;
 
 mongoose
   .connect(mongodbUrl, {
@@ -26,6 +27,6 @@ app.use("/api/users", userRoute);
 app.use("/api/products", productRoute);
 app.use("/api/orders", orderRoute);
 
-app.listen(process.env.PORT || 5000, () => {
-  console.log("Server started at http://localhost:5000");
+app.listen(PORT, () => {
+  console.log(`Server started at ${PORT}`);
 });
